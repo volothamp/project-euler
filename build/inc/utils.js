85234c21bde333c2cdbfa23651c332621afeb860
Volothamp Geddarm <volocodes@gmail.com>
2024-10-12 22:01:43 -0400

    Removed build/ and updated .gitignore

--- build/inc/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.inputToNumber = inputToNumber;
-//If input is a string that can be converted to an integer, return the integer. Otherwise return false.
-function inputToNumber(input) {
-    var num = parseInt(input, 10);
-    return isNaN(num) ? false : num;
-}
