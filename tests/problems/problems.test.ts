2b6e29d95ef19534f2113146e54f1823a96966c5
Volothamp Geddarm <volocodes@gmail.com>
2024-10-14 13:01:51 -0400

    Adds problem 6 solution and tests

--- tests/problems/problems.test.ts
+++ tests/problems/problems.test.ts
@@ -1,5 +1,6 @@
 import { getFibonacciSequence } from '../../src/problems/prob-2';
 import { getLargestPalindromeProduct } from '../../src/problems/prob-4';
+import * as p6 from '../../src/problems/prob-6';
 
 describe('Problem Specific Functions', () => {
   describe('Problem 2', () => {
@@ -18,6 +19,18 @@ describe('Problem Specific Functions', () => {
       test('min: 10, max: 99', () => {
         expect(getLargestPalindromeProduct(10, 99)).toBe(9009);
       });
+    });
+  });
+
+  describe('Problem 6', () => {
+    test('sum of squares of 1-10 should be 385', () => {
+      expect(p6.getSumOfSquares([1,2,3,4,5,6,7,8,9,10])).toBe(385);
+    });
+    test('square of sum of 1-10 should be 3025', () => {
+      expect(p6.getSquareOfSum([1,2,3,4,5,6,7,8,9,10])).toBe(3025);
+    });
+    test('difference between sum of squares and square of sums of 1-10 should be 2640', () => {
+      expect(p6.problem6([1,2,3,4,5,6,7,8,9,10])).toBe(2640);
     });
   });
 });
